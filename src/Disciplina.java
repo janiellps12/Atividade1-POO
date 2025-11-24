19dcdd5b9564e4f6dfcb27f28be9a2fc90cb4d81
Chris <gatinhochristian448@gmail.com>
2025-11-23 22:14:57 -0300

    criação da classe disciplina

---
+++ src/Disciplina.java
@@ -0,0 +1,16 @@
+public class Disciplina {
+    private String codigo;
+    private String titulo;
+    private int capacidadeMaxima;
+}
+//verificar se a disciplina atingiu sua capacidade máxima
+
+
+
+
+
+
+
+
+
+
